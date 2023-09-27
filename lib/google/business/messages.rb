7c01b8d0035835d2af24dc99ce3896e8ece94481
marcus.salinas <12.marcus.salinas@gmail.com>
2023-09-27 10:39:19 +0400

    init

---
+++ lib/google/business/messages.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require_relative "messages/version"
+
+module Google
+  module Business
+    module Messages
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
